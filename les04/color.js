1525c786f9a2e427cc15dee179def5ff11297c4f
natalie <natalie.like.it@gmail.com>
2020-08-31 22:52:56 +0300

    added task4

---
+++ les04/color.js
@@ -0,0 +1,9 @@
+
+function changeBackgroungcolor(id) {
+let color1 = document.getElementsByClassName('color');
+// console.log(color1)
+// console.log(id)
+document.body.style.backgroundColor = color1[id].style.backgroundColor;   
+};
+
+
--- les04/main.js
+++ les04/main.js
@@ -1,7 +1,6 @@
 // Task 1
 
 const rooms = [['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9], ['XXXXXX', 9]];
-
 function meeting (rooms, needsChairs) {
   if (needsChairs <= 0) {
       console.log('Game On');
@@ -27,23 +26,14 @@ function meeting (rooms, needsChairs) {
  console.log(result);
 }
 
-meeting(rooms, 5);
+meeting(rooms, 3);
 
-// Task 2
-const newArr = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+// // Task 2
+
 let area = document.getElementById('area');
 let step = 0;
-let startButton = document.getElementById('.start');
-area.addEventListener('click', function(event) {
+
+area.addEventListener('click', function(event) { 
   if(step % 2 === 0) {
     player = 'X'
     event.target.innerHTML = player;
@@ -56,12 +46,23 @@ area.addEventListener('click', function(event) {
 })
 
 function check(player){
+    const newArr = [ //массив выигрышных операций
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
   const cells = document.getElementsByClassName('input');
   let result = "";
   for(let i = 0; i < newArr.length; i++) {
   if(cells[newArr[i][0]].innerHTML == player 
   && cells[newArr[i][1]].innerHTML == player
-   && cells[newArr[i][2]].innerHTML == player){
+   && cells[newArr[i][2]].innerHTML == player) 
+   {
     result = player;
    } else if(step == 9) {
      result = 0
@@ -85,11 +86,32 @@ function check(player){
       break;
   }
 };
-function game_reset (cells) {
+
+
+function game_reset (cells) { //начинает игру заново
   for(let i=0; i<cells.length; i++){
     cells[i].innerHTML = "";
     step = 0;
   }
-}
+};
 
 // Task 3
+
+
+let test = ['apple', 'cherry', 'melon', 'pear', 'watermelon'];
+// test.splice(0, 1); // shift - извлекает элемент из начала
+// console.log(test);
+
+// test.splice(4, 1); // pop - извлекает элемент из конца
+// console.log(test);
+
+// test.splice(0, 2, 'pizza', 'cake');
+// console.log(test);
+
+// test.splice(0, 0, 'sprite', 'cola');// unshift - добавляет элементы в начало
+// console.log(test);
+
+test.splice(5, 0, 'sprite', 'cola');// push - добавляет элементы в конец
+console.log(test);
+
+// concat - создаёт новый массив, в который копирует данные из других массивов и дополнительные значения
